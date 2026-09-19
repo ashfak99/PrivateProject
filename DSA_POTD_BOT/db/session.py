c83154143d0fdf51fff0521e2de2ff03b45c8186
Ashfak Alam <aliakbaralam86@gmail.com>
2026-09-20 02:52:16 +0530

    SSL mode

--- DSA_POTD_BOT/db/session.py
+++ DSA_POTD_BOT/db/session.py
@@ -4,7 +4,16 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
+import ssl
+
+
 from config.settings import settings
+
+# Aiven ke liye SSL context
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
 
 engine = create_async_engine(
     settings.DATABASE_URL,
